0445a92c0d6c3a94524f8908552a76ed4d65773d
MEHAK LAWANIA <85499002+mehaklawania@users.noreply.github.com>
2024-12-08 05:02:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_GOOGLE_AI_KEY: process.env.NEXT_PUBLIC_GOOGLE_AI_KEY,
+  },
+};
 
-module.exports = {
-    env: {
-      NEXT_PUBLIC_GOOGLE_AI_KEY: process.env.NEXT_PUBLIC_GOOGLE_AI_KEY,
-    },
-  };
+export default nextConfig;
